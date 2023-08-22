e4d90c0aaf9345c367e53f65ea237109c7942538
scottwilson <scott.bradley.wilson@gmail.com>
2023-08-22 07:47:43 +0100

    fix: Updated distribution scripts

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='tableau_builder',
-    version='0.5',
+    version='0.6',
     packages=['tableau_builder'],
     url='https://github.com/JiscDACT/tableau-builder',
     license='',
