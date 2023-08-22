e015aece12410904d212cf3575c3272d3fd39c8a
scottwilson <scott.bradley.wilson@gmail.com>
2023-08-22 07:25:59 +0100

    feat: Enable manylinux dist

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='tableau_builder',
-    version='0.4',
+    version='0.5',
     packages=['tableau_builder'],
     url='https://github.com/JiscDACT/tableau-builder',
     license='',
